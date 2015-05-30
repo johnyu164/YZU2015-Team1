bd00607f35ea90bda70f87889d7af0e1c4e5dade
teacher <teacher@yzu.edu.tw>
2015-05-30 10:28:17 +0800

    failed test

--- Team1/Team1/LibraryTest.cs
+++ Team1/Team1/LibraryTest.cs
@@ -9,6 +9,13 @@ namespace Team1
         [TestMethod]
         public void FindBookbyNumber()
         {
+            FindBook fi = new FindBook();
+            fi.ReadData("Library.txt");
+
+            BookInformation info = fi.FindBookbyNumber("00001");
+            Assert.AreEqual("", info.booknumber);
+            Assert.AreEqual("", info.bookname);
+            Assert.AreEqual("", info.borrowornot);
         }
     }
 }
