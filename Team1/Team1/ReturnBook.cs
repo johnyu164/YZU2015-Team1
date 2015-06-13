9918fa2beef2f606ce56e6dd5087d1408461cf66
123 <123@qwe>
2015-06-13 10:32:33 +0800

    test4

--- Team1/Team1/ReturnBook.cs
+++ Team1/Team1/ReturnBook.cs
@@ -27,7 +27,7 @@ namespace Team1
 
         public string Load_Number_for_borrow(String number)
         {
-            StreamReader sr = new StreamReader("..\\borrow.txt", Encoding.Default);
+            StreamReader sr = new StreamReader("\\borrow.txt", Encoding.Default);
             String line;
             while ((line = sr.ReadLine()) != null)
             {
@@ -44,7 +44,7 @@ namespace Team1
 
         public string getUser_for_ReturnBook(String account)
         {
-            StreamReader sr = new StreamReader("..\\..\\User.txt", Encoding.Default);
+            StreamReader sr = new StreamReader("\\User.txt", Encoding.Default);
             String line;
             while ((line = sr.ReadLine()) != null)
             {
